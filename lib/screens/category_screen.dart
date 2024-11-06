d916c0b2e41c378ad9aa5fe16e3db8e900676cd6
Namalwamillia <gracenamalwa019@gmail.com>
2024-11-06 20:01:16 +0300

    changes

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,66 +1,78 @@
 
-
 import 'package:dart_models/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class CategoryScreen extends StatelessWidget {
   CategoryScreen({super.key});
+
   List<CategoryModel> categories = [
     CategoryModel(
         id: 1,
-        name: 'Nike Air Max',
+        name: 'Soap',
         image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7bv863BlXSS9cVbgum9uFETX7vToO9NLn_m2eHA70fUFDJPjd6RC-QvquFUseSKoNKo0&usqp=CAU",
-        description: 'Comfortable and stylish, perfect for everyday wear.'),
-
+            'https://thumbs.dreamstime.com/b/composition-different-bars-soap-lot-toilet-set-various-soaps-279756246.jpg?w=360',
+        description: 'Freshy for bathing'),
     CategoryModel(
         id: 2,
-        name: 'Adidas Ultraboost',
+        name: 'Oil',
         image:
-            "https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU=",
-        description: 'High-performance running shoe with maximum cushioning.'),
-
+            'https://thumbs.dreamstime.com/b/colored-plastic-bottles-liquid-soap-set-shower-gel-isolated-white-background-studio-shooting-set-49371187.jpg?w=768',
+        description: 'smoothening your body'),
     CategoryModel(
         id: 3,
-        name: 'Puma Suede Classic',
+        name: 'Flour',
         image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ27IGmr79S1FY3hxmRovI-4AjsLazb_7nAWA&s",
-        description: 'Iconic design with a casual look for versatile style.'),
-
+            'https://thumbs.dreamstime.com/b/glass-bowl-corn-starch-isolated-white-93967111.jpg?w=360',
+        description: 'delicious'),
     CategoryModel(
         id: 4,
-        name: 'Reebok Nano X1',
+        name: '  Soap',
         image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsTPudja0GD4PCLR3IoOYA6Hu8GgpgatxRApvSvLl19-RLFIxFB8N6pBeh5amNwrUB5fM&usqp=CAU",
-        description: 'Ideal for cross-training, offering stability and support.'),
-
+            'https://thumbs.dreamstime.com/b/yellow-bar-soap-1687920.jpg?w=768',
+        description: ''),
+    CategoryModel(
+        id: 5,
+        name: 'Vaccine',
+        image:
+            'https://m.media-amazon.com/images/I/413kKRX0QTL._AC_UF480,480_SR480,480_.jpg',
+        description: ''),
+    CategoryModel(
+        id: 6,
+        name: 'Odi',
+        image:
+            'https://thumbs.dreamstime.com/b/peanut-butter-spoon-white-background-37964382.jpg?w=768',
+        description: '')
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Categories'),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            final category = categories[index];
-            return ListTile(
-              leading: Image.network(
-                category.image,
-                width: 100,
-                height:100,
-                fit: BoxFit.cover,
-              ),
-              title: Text(category.name),
-              subtitle: Text(category.description ?? 'No description available'),
-            );
-          }),
-    );
+        appBar: AppBar(
+          title: const Text('Categories'),
+          backgroundColor: Colors.lightBlue,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+         
+        ),
+        body: ListView.builder(
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(categories[index].image))),
+                ),
+                title: Text(categories[index].name),
+              subtitle: Text(categories[index].description ?? 'No description available'),
+  
+              );
+            }));
   }
 }
